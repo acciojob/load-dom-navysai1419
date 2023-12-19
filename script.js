97ed2b09a02586feba098c8cf055c8e135930d36
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-19 12:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-//your JS code here. If required.
-        // Function to be executed when DOM is loaded
-        function onDomLoad() {
-            // Create a new text node with the message
-            var textNode = document.createTextNode("DOM load success");
+// script.js
 
-            // Create a new paragraph element
-            var paragraph = document.createElement("p");
+// Create a new paragraph element
+var paragraph = document.createElement("p");
 
-            // Append the text node to the paragraph element
-            paragraph.appendChild(textNode);
+// Set the text content of the paragraph
+paragraph.textContent = "DOM load success";
 
-            // Append the paragraph to the body
-            document.body.appendChild(paragraph);
-        }
-
-        // Add an event listener for DOMContentLoaded event
-        document.addEventListener("DOMContentLoaded", onDomLoad);
+// Append the paragraph to the body
+document.body.appendChild(paragraph);
