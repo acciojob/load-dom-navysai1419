cfaa8a55b5c71a25af9823ed8944736d5e1f4374
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 18:39:18 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-            // DOM is loaded, write the string to a new element
-            var messageElement = document.createElement('div');
-            messageElement.innerHTML = 'DOM load success';
-            document.body.appendChild(messageElement);
-        });
